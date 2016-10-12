4a762a053ff146581ba7443d99f667f73bd2bdf0
관리자 <gwanlija@gwanlijaui-iMac-30.local>
2016-10-12 11:39:20 +0900

    first plot ok

--- Assignment2/test_myran.m
+++ Assignment2/test_myran.m
@@ -2,13 +2,17 @@
 function test_myran()
 
 % generate 1,000 random numbers from your ¡°myran.m¡± and store them in the memory
-N = 1000;
-result = myran(N);
+N = 100000;
+data = myran(N);
 
 % Then, you calculate and draw probability density function of your random number sequence. 
+bin_width = 0.01;
+xRange = 0:bin_width:1;
+N = hist(data, xRange);
+nbins = 1 / bin_width;
+plot(xRange, N.*nbins./numel(data));
 
 
-% The size of the bin is set to be 0.1. 
 % Does it look uniform distribution to you?
 % Does it match with the expected value of the probability density function? 
 % If it doesn¡¯t, why?
