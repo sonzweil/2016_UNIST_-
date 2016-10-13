b1d1cb02f916a3392d5e4ee761eb5e4fd5956cfb
관리자 <gwanlija@gwanlijaui-iMac-5.local>
2016-10-13 21:46:36 +0900

    Question filled

--- Assignment2/myran.m
+++ Assignment2/myran.m
@@ -1,23 +1,19 @@
 % Q1) 
-% Suppose we have a uniform distribution of random numbers, x, in X ? [a, b]. What are the 
-% probability density function : 
-% the mean :
-% the variance :
-
-% When a = 0 and b = 1
-% the moment of the random number sequence : 
+% Suppose we have a uniform distribution of random numbers, x, in [a, b]. 
+% What are the probability density function p(x) = 1 / (b - a)
+% the mean <x> = (a + b) / 2
+% the variance <x^2> - <x>^2 = (a^2 - 3*a*b + b^2) / 12
+% When a = 0 and b = 1, the moment of the random number sequence <x^m> = 1 / (m + 1)
 
 % Q2)
 % take an integer number N as an input variable and spits out a N random number sequence between 0 and 1.
-
 function result = myran(N)
 
 result = zeros(N, 1);
+
 for i = 1:N
     result(i) = rand();
 end
-
-result;
 
 end
 
