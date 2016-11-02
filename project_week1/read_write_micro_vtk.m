5d02726fb6ce6509ed38899197043b522484abea
sonzweil <smartsonzweil@gmail.com>
2016-11-02 16:29:10 +0900

    project_week1 finished

---
+++ project_week1/read_write_micro_vtk.m
@@ -0,0 +1,27 @@
+function read_write_micro_vtk()
+
+data = zeros(1, 10000);
+
+readFile = fopen('test_micro.vtk','r');
+writeFile = fopen('test_micro_1000.vtk','w');
+
+for index = 1:215
+    str = fgets(readFile);
+    fprintf(writeFile, '%s', str);
+end
+
+for i = 1:1000
+    str = fgets(readFile);
+    data = sscanf(str,'%d %d %d %d %d %d %d %d %d %d');
+    
+    for j = 1:10
+        data(j, 1) = data(j, 1) + 1000;
+    end
+    
+    fprintf(writeFile, '%7d', data);
+    fprintf(writeFile, '\n');
+end
+
+
+end
+
